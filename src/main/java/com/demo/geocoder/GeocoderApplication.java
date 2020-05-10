c04cfff81181dc90fc1b25e5d0371d0cab1f5d1a
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2020-05-10 21:18:43 +0300

    Создание шаблона проекта

---
+++ src/main/java/com/demo/geocoder/GeocoderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.geocoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/geocoder/GeocoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.geocoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
